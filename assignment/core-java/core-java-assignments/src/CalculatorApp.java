2e32fbc3aec61bdc14c55fd4d183ae2e13ccc95c
VijayKumarBandi1 <vijaykumar.bandi@indiumsoft.com>
2023-08-29 11:13:56 +0530

    calculator

---
+++ assignment/core-java/core-java-assignments/src/CalculatorApp.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class CalculatorApp {
+
+	public static void main(String[] args)
+	   {
+		int choice;
+	      calculator();
+	      Scanner scan = new Scanner(System.in);
+	      do {
+	      System.out.println("\n1. continue");
+	      System.out.println("2. exit");
+	      System.out.print("\nEnter Your Choice (1-2):\n ");
+	      choice = scan.nextInt();
+	      if(choice<=2) {
+	    	  if(choice==1) {
+	    		  calculator();
+	    	  }
+	    	  else {
+	    		  System.out.println("\nGood Bye!! Have a nice day");
+	    	  }
+	      }
+	      else
+	    	  System.out.println("\nInvalid Choice!\n");
+	      }while(choice==1);
+	   } 
+
+	public static void calculator() {
+		float a, b, res;
+	      int choice;
+	      Scanner scan = new Scanner(System.in);
+	      
+	      
+	      System.out.println("1. Addition(+)");
+	      System.out.println("2. Subtraction(-)");
+	      System.out.println("3. Multiplication(*)");
+	      System.out.println("4. Division(/)");
+	      System.out.print("Enter Your Choice (1-4): ");
+	      choice = scan.nextInt();
+	      
+	      if(choice>=1 && choice<=4)
+	      {
+	         System.out.print("\nEnter First Number: ");
+	         a = scan.nextFloat();
+	         System.out.print("Enter Second Number: ");
+	         b = scan.nextFloat();
+	         
+	         if(choice==1)
+	            res = a+b;
+	         else if(choice==2)
+	            res = a-b;
+	         else if(choice==3)
+	            res = a*b;
+	         else
+	            res = a/b;
+	         
+	         System.out.println("\nResult = " +res);
+	      }
+	      else
+	         System.out.println("\nInvalid Choice!");
+	}
+	
+}
