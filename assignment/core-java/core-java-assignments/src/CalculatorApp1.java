4404fc024d70c27be0bd1710d6d51242b09328c3
VijayKumarBandi1 <vijaykumar.bandi@indiumsoft.com>
2023-08-30 12:05:54 +0530

    simple calculator

---
+++ assignment/core-java/core-java-assignments/src/CalculatorApp1.java
@@ -0,0 +1,22 @@
+public class CalculatorApp1 {
+
+	public static void main(String[] args) {
+
+		float num1 = 30f;
+		float num2 = 40f;
+		
+		//addition
+		System.out.println("Addition: " + (num1+num2));
+		
+		//subtraction
+		System.out.println("subtraction: " + (num1-num2));
+		
+	    //multiplication
+		System.out.println("multiplication: " + (num1*num2));
+		
+		//Division
+		System.out.println("Division: " + (num1/num2));
+
+	}
+
+}
--- assignment/core-java/core-java-assignments/src/CalculatorApp2.java
+++ assignment/core-java/core-java-assignments/src/CalculatorApp2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculatorApp {
+public class CalculatorApp2 {
 
     public static void main(String[] args) {
         int choice;
